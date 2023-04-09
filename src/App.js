e11968593db6b24f7070a5fb4c248478b3c80dfd
dahnedewy <121363790+dahnedewy@users.noreply.github.com>
2023-04-09 14:15:40 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Josephine Marecha and is {""}
-        <a href="https://github.com/dahnedewy/react-weather-app" target="blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Josephine Marecha and is {""}
+          <a
+            href="https://github.com/dahnedewy/react-weather-app"
+            target="blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
