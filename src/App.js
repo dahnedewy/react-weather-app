1a74292a9a9d4d2b559319df8c12b29d6c8a1bd6
dahnedewy <121363790+dahnedewy@users.noreply.github.com>
2023-04-09 11:19:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by Josephine Marecha and is{""}
+        This project was coded by Josephine Marecha and is {""}
         <a href="https://github.com/dahnedewy/react-weather-app" target="blank">
           open-sourced on GitHub
         </a>
