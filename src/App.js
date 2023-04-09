ebe6cd1dcc655a8de3ace2d719fbe7d4d5ffd33d
dahnedewy <121363790+dahnedewy@users.noreply.github.com>
2023-04-09 11:17:14 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Josephine Marecha and is{""}
+        <a href="https://github.com/dahnedewy/react-weather-app" target="blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
